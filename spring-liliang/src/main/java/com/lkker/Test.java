ffed6facfa30d157ebe482adc96a4fab66541559
liliang <dzliliang@126.com>
2019-08-10 16:10:38 +0800

    commit

--- spring-liliang/src/main/java/com/lkker/Test.java
+++ spring-liliang/src/main/java/com/lkker/Test.java
@@ -1,5 +1,9 @@
 package com.lkker;
 
+import com.lkker.config.JavaConfig;
+import com.lkker.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @Author liliang
  * @Date 2019/8/9 21:52
@@ -8,6 +12,10 @@ package com.lkker;
 public class Test {
 
 	public static void main(String[] args) {
+		AnnotationConfigApplicationContext atx = new AnnotationConfigApplicationContext(JavaConfig.class);
 
+		Object bean = atx.getBean("userServiceImpl", UserService.class);
+
+		System.out.println("beanName:"+bean.getClass().getName());
 	}
 }
---
+++ spring-liliang/src/main/java/com/lkker/service/LogisticsService.java
@@ -0,0 +1,11 @@
+package com.lkker.service;
+
+/**
+ * @Author liliang
+ * @Date 2019/8/10 16:01
+ * @Description
+ **/
+public interface LogisticsService {
+
+	void save();
+}
