2fc3a999e4d393757c689f2b8b07cf93a2f2fba2
liliang <dzliliang@126.com>
2020-03-24 23:46:52 +0800

    springmvc内嵌一个tomcat

--- spring-web/src/main/java/org/springframework/liliang/config/AppConfig.java
+++ spring-web/src/main/java/org/springframework/liliang/config/AppConfig.java
@@ -1,7 +1,11 @@
 package org.springframework.liliang.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
+@ComponentScan({"org.springframework.liliang"})
 public class AppConfig {
 }
