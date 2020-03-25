4007d3c1e1349abee50fcfe4596e2d1ac6eafee1
liliang <dzliliang@126.com>
2020-03-25 23:46:48 +0800

    springmvc内嵌一个tomcat

--- spring-web/src/main/java/org/springframework/liliang/config/MyWebApplicationInitializer.java
+++ spring-web/src/main/java/org/springframework/liliang/config/MyWebApplicationInitializer.java
@@ -10,7 +10,7 @@ public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatc
 	 */
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[0];
+		return new Class[]{RootConfig.class};
 	}
 
 	/**
