c87361901c1f54f4c207756ce79d01aa869a79fc
liliang <dzliliang@126.com>
2020-03-30 23:21:27 +0800

    添加构造函数

--- spring-liliang-mvc/src/main/java/com/lkker/config/AppConfig.java
+++ spring-liliang-mvc/src/main/java/com/lkker/config/AppConfig.java
@@ -22,44 +22,43 @@ import java.util.List;
 		@ComponentScan.Filter(type = FilterType.ANNOTATION,value = {RestController.class, Controller.class})
 },useDefaultFilters = false)
 public class AppConfig implements WebMvcConfigurer {
-	public AppConfig() {
-	}
-	/**
-	 * 配置拦截器
-	 */
 
-
-	/**
-	 * 配置文件上传下载的组件
-	 * @return
-	 */
-	@Bean
-	public MultipartResolver multipartResolver(){
-		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-		multipartResolver.setDefaultEncoding("UTF-8");
-		multipartResolver.setMaxUploadSize(1024*1024*10);
-		return multipartResolver;
-	}
-
-	/**
-	 * 配置视图解析器
-	 * @return
-	 */
-	@Bean
-	public InternalResourceViewResolver internalResourceViewResolver(){
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/jsp");
-		resolver.setSuffix(".jsp");
-		return resolver;
-
-	}
-
-	/**
-	 * 重写消息转换器
-	 * @param converters initially an empty list of converters
-	 */
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(new MappingJackson2HttpMessageConverter());
-	}
+//	/**
+//	 * 配置拦截器
+//	 */
+//
+//
+//	/**
+//	 * 配置文件上传下载的组件
+//	 * @return
+//	 */
+//	@Bean
+//	public MultipartResolver multipartResolver(){
+//		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+//		multipartResolver.setDefaultEncoding("UTF-8");
+//		multipartResolver.setMaxUploadSize(1024*1024*10);
+//		return multipartResolver;
+//	}
+//
+//	/**
+//	 * 配置视图解析器
+//	 * @return
+//	 */
+//	@Bean
+//	public InternalResourceViewResolver internalResourceViewResolver(){
+//		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//		resolver.setPrefix("/WEB-INF/jsp");
+//		resolver.setSuffix(".jsp");
+//		return resolver;
+//
+//	}
+//
+//	/**
+//	 * 重写消息转换器
+//	 * @param converters initially an empty list of converters
+//	 */
+//	@Override
+//	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//		converters.add(new MappingJackson2HttpMessageConverter());
+//	}
 }
