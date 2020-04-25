13f6d6577ec5770a909cd0a7cd2664d3ef2dc5b4
liliang <dzliliang@126.com>
2020-04-25 20:36:33 +0800

    commit

--- spring-webmvc/src/main/java/org/springframework/web/servlet/handler/MatchableHandlerMapping.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/handler/MatchableHandlerMapping.java
@@ -34,6 +34,7 @@ public interface MatchableHandlerMapping extends HandlerMapping {
 
 	/**
 	 * Determine whether the given request matches the request criteria.
+	 * 确定给定的请求是否符合请求条件。
 	 * @param request the current request
 	 * @param pattern the pattern to match
 	 * @return the result from request matching, or {@code null} if none
