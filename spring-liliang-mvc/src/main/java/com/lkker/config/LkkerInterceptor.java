add8263d4445b6a2a22ceb244c2702971e6be3e2
liliang <liliang@laiyuezs.com>
2020-05-06 17:15:31 +0800

    新增拦截器

---
+++ spring-liliang-mvc/src/main/java/com/lkker/config/LkkerInterceptor.java
@@ -0,0 +1,34 @@
+package com.lkker.config;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author liliang
+ * @Date 2020/5/6 17:10
+ * @Description
+ **/
+@Component
+public class LkkerInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("preHandle...............");
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		System.out.println("postHandle.................");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		System.out.println("afterCompletion.............");
+	}
+}
