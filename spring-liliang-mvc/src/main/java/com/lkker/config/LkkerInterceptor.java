ef38aa73363235cd15e7794491d6dcf5be7f60fa
liliang <liliang@laiyuezs.com>
2020-05-06 17:42:26 +0800

    commit

--- spring-liliang-mvc/src/main/java/com/lkker/config/LkkerInterceptor.java
+++ spring-liliang-mvc/src/main/java/com/lkker/config/LkkerInterceptor.java
@@ -19,7 +19,7 @@ public class LkkerInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		System.out.println("preHandle...............");
-		return false;
+		return true;
 	}
 
 	@Override
