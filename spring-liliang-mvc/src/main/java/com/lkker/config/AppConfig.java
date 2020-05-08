01c060b2a0df9bfa60799bd765773ea1b81339b8
liliang <liliang@laiyuezs.com>
2020-05-08 16:29:45 +0800

    在 SpringMVC 中，使用 jackson 处理json字符串

--- spring-liliang-mvc/src/main/java/com/lkker/config/AppConfig.java
+++ spring-liliang-mvc/src/main/java/com/lkker/config/AppConfig.java
@@ -1,17 +1,13 @@
 package com.lkker.config;
 
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @ComponentScan(value = {"com.lkker"},includeFilters = {
@@ -27,15 +23,6 @@ public class AppConfig  implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		LkkerInterceptor lkkerInterceptor = new LkkerInterceptor();
 		registry.addInterceptor(lkkerInterceptor).addPathPatterns("/**");
-	}
-
-	/**
-	 * 重写消息转换器
-	 * @param converters initially an empty list of converters
-	 */
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(new FastJsonHttpMessageConverter());
 	}
 
 
