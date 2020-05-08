d2de1e096627d891813e13b003d42935bc2a8fc1
liliang <liliang@laiyuezs.com>
2020-05-08 15:18:25 +0800

    commit - 增加post方法

--- spring-liliang-mvc/src/main/java/com/lkker/controller/LkkerController.java
+++ spring-liliang-mvc/src/main/java/com/lkker/controller/LkkerController.java
@@ -3,10 +3,7 @@ package com.lkker.controller;
 import com.lkker.model.UserInfo;
 import com.lkker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LkkerController {
@@ -18,6 +15,12 @@ public class LkkerController {
 	public UserInfo UserInfo(@PathVariable("userId") String userId){
 		UserInfo userInfo = userService.getUserInfo(userId);
 		return userInfo;
+	}
+
+	@RequestMapping(value = "/addUser",method = RequestMethod.POST)
+	public UserInfo addUser(@RequestBody UserInfo userInfo){
+		UserInfo user = userInfo;
+		return user;
 	}
 
 }
